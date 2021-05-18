80d869436d238b86f6c3b8b00caf37a11f84cfaa
antony-jr <antonyjr@protonmail.com>
2021-05-18 21:02:19 +0530

    [skip ci] add get script.

---
+++ scripts/get-v0.1.2.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo wget -O /usr/bin/spirit "https://github.com/antony-jr/spirit/releases/download/v0.1.2/spirit"
+sudo chmod +x /usr/bin/spirit
+
+echo "Installed Spirit v0.1.2!"
